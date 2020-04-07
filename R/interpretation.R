032a67332bdc60a56a2e96be2ea8e15dd55208e0
Philip Leifeld <philip.leifeld@essex.ac.uk>
2020-04-07 11:20:02 +0000

    version 1.9.7

--- R/interpretation.R
+++ R/interpretation.R
@@ -99,10 +99,8 @@ dointerpret <- function(l, coefficients, type, i, j, t) {
     results <- list()
     for (i in t) {
       # print error if undirected
-      if ((class(l$networks[[i]]) == "network" && 
-          !is.directed(l$networks[[i]])) || 
-          (class(l$networks[[i]]) == "matrix" && 
-          is.mat.directed(l$networks[[i]]) == FALSE)) {
+      if ((is.network(l$networks[[i]]) && !is.directed(l$networks[[i]])) || 
+          (is.matrix(l$networks[[i]]) && is.mat.directed(l$networks[[i]]) == FALSE)) {
         stop(paste0("Network at t=", i, " is undirected. Dyadic ", 
             "probabilities do not make sense in undirected networks. Try ", 
             "type = \"tie\" instead!"))
@@ -225,10 +223,8 @@ interpret.ergm <- function(object, formula = getformula(object),
   }
   
   # warn about indices with bipartite networks
-  if (class(l$networks[[1]]) == "network" 
-      && is.bipartite(l$networks[[1]]) || 
-      class(l$networks[[1]]) == "matrix" 
-      && !is.mat.onemode(l$networks[[1]])) {
+  if (is.network(l$networks[[1]]) && is.bipartite(l$networks[[1]]) || 
+      is.matrix(l$networks[[1]]) && !is.mat.onemode(l$networks[[1]])) {
     if (is.numeric(j) && j < nrow(as.matrix(l$networks[[1]])) + 1) {
       stop(paste0("In this bipartite network, the indices of the 'j' ", 
           "index start with ", nrow(as.matrix(l$networks[[1]])) + 1, 
@@ -257,9 +253,8 @@ interpret.btergm <- function(object, formula = getformula(object),
   
   # warn about indices with bipartite networks
   for (h in t) {
-    if (class(l$networks[[h]]) == "network" && is.bipartite(l$networks[[h]]) || 
-        class(l$networks[[h]]) == "matrix" 
-        && !is.mat.onemode(l$networks[[h]])) {
+    if (is.network(l$networks[[h]]) && is.bipartite(l$networks[[h]]) || 
+        is.matrix(l$networks[[h]]) && !is.mat.onemode(l$networks[[h]])) {
       if (is.numeric(j) && j < nrow(as.matrix(l$networks[[h]])) + 1) {
         stop(paste0("In this bipartite network, the indices of the 'j' ", 
             "index start with ", nrow(as.matrix(l$networks[[h]])) + 1, 
@@ -286,12 +281,11 @@ setMethod("interpret", signature = className("mtergm", "btergm"),
 
 
 # a function that creates all tie probabilities along with some other variables
-edgeprob <- function (object, verbose = FALSE)
-{
-  if (class(object) == "ergm") {
+edgeprob <- function (object, verbose = FALSE) {
+  if ("ergm" %in% class(object)) {
     tergm <- FALSE
   }
-  else if (class(object) %in% c("btergm", "mtergm")) {
+  else if ("btergm" %in% class(object) || "mtergm" %in% class(object)) {
     tergm <- TRUE
   }
   else {
@@ -304,9 +298,9 @@ edgeprob <- function (object, verbose = FALSE)
     assign(l$covnames[cv], l[[l$covnames[cv]]])
   }
   assign("offsmat", l$offsmat)
-  form <- as.formula(l$form)
+  form <- stats::as.formula(l$form)
   covnames <- l$covnames[-1]
-  coefs <- coef(object)
+  coefs <- stats::coef(object)
   if (verbose == TRUE) {
     message("Creating data frame with predictors...")
   }
@@ -315,9 +309,8 @@ edgeprob <- function (object, verbose = FALSE)
   for (i in 1:length(l$networks)) {
     mat <- as.matrix(l$networks[[i]])
     imat <- matrix(rep(1:nrow(mat), ncol(mat)), nrow = nrow(mat))
-    if ((class(l$networks[[i]]) == "network" && is.bipartite(l$networks[[i]])) ||
-        (class(l$networks[[i]]) == "matrix" && is.mat.onemode(l$networks[[i]]) ==
-         FALSE)) {
+    if ((is.network(l$networks[[i]]) && is.bipartite(l$networks[[i]])) ||
+        (is.matrix(l$networks[[i]]) && is.mat.onemode(l$networks[[i]]) == FALSE)) {
       mn <- nrow(mat) + 1
       mx <- nrow(mat) + ncol(mat)
       jmat <- matrix(rep(mn:mx, nrow(mat)), nrow = nrow(mat),
@@ -327,13 +320,12 @@ edgeprob <- function (object, verbose = FALSE)
       jmat <- matrix(rep(1:ncol(mat), nrow(mat)), nrow = nrow(mat),
                      byrow = TRUE)
     }
-    f <- as.formula(paste(l$form, " + edgecov(imat) + edgecov(jmat)"))
+    f <- stats::as.formula(paste(l$form, " + edgecov(imat) + edgecov(jmat)"))
     mpli <- ergm::ergmMPLE(f)
     Y <- c(Y, mpli$response)
     dyads <- rbind(dyads, cbind(mpli$predictor, i))
   }
-  term.names <- colnames(dyads)[-(length(colnames(dyads)):(length(colnames(dyads)) -
-                                                             2))]
+  term.names <- colnames(dyads)[-(length(colnames(dyads)):(length(colnames(dyads)) - 2))]
   term.names <- c(term.names, "i", "j", "t")
   dyads <- data.frame(dyads)
   colnames(dyads) <- term.names
@@ -342,7 +334,7 @@ edgeprob <- function (object, verbose = FALSE)
   class(dyads[, length(colnames(dyads))]) <- "integer"
   class(dyads[, length(colnames(dyads)) - 1]) <- "integer"
   class(dyads[, length(colnames(dyads)) - 2]) <- "integer"
-  cf <- coef(object)
+  cf <- stats::coef(object)
   cf.length <- length(cf)
   cf <- cf[!cf %in% c(Inf, -Inf)]
   if (length(cf) != cf.length) {
@@ -352,13 +344,21 @@ edgeprob <- function (object, verbose = FALSE)
   }
   cbcoef <- cbind(cf)
   chgstat <- dyads[, 2:(ncol(dyads) - 3)]
+  ##handle decay term in curved ergms
+  if(ergm::is.curved(object)){
+    curved.term<-vector(length=length(object$etamap$curved))
+    for(i in 1:length(object$etamap$curved)){
+    curved.term[i]<-object$etamap$curved[[i]]$from[2]
+    }
+    cbcoef<-cbcoef[-c(curved.term)]
+  }
+
   lp <- apply(chgstat, 1, function(x) t(x) %*% cbcoef)
   result <- c(1/(1 + exp(-lp)))
-  # add in vertex ids
   i.name <- numeric(nrow(dyads))
   j.name <- numeric(nrow(dyads))
   for (t in 1:length(l$networks)) {
-    vnames.t <- colnames(l$networks[[i]][, ])
+    vnames.t <- colnames(l$networks[[t]][, ])
     dyads.t <- dyads[which(dyads$t == t), ]
     i.name.t <- vnames.t[dyads.t$i]
     j.name.t <- vnames.t[dyads.t$j]
@@ -373,6 +373,7 @@ edgeprob <- function (object, verbose = FALSE)
   rownames(dyads) <- NULL
   return(dyads)
 }
+
 
 # function for marginal effects plots
 marginalplot <- function(model, var1, var2, inter, ci = 0.95, 
